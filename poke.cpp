7860ad1a693b450f4f74b142dccc736ea2d11f32
NordiaG <nikprakash20@gmail.com>
2022-04-19 00:39:59 -0400

    debug output for OPTTSP

--- poke.cpp
+++ poke.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	path graph;
 	cerr << fixed << showpoint << setprecision(2);
-	cout << setprecision(2);
+	cout << setprecision(2) << boolalpha;
 	cout << fixed;
 	graph.getOptions(argc, argv);
 
